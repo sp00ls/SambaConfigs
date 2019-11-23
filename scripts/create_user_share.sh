85a24f20a37a0852a23899477804474a6ecb77fc
Ken <ken@sp00l.org>
2019-11-23 00:44:35 -0500

    Add public share symlink creation

--- scripts/create_user_share.sh
+++ scripts/create_user_share.sh
@@ -4,5 +4,6 @@ if [ ! -e /tank/user/$1 ]; then
 	zfs create tank/user/$1
 	chown $1:"shares" /tank/user/$1
     chmod -R 700 $1 /tank/user/$1
+    ln -s /tank/public /tank/user/$1/Public
 fi
 exit 0
